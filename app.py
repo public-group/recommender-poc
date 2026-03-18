7c0ff61259f38df5656c45b9e662cf81798357d7
public-ktolia <k.tolia@pmmretail.com>
2026-03-18 14:41:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -557,7 +557,7 @@ st.sidebar.markdown(f"""
         color: #333 !important;
         white-space: pre-line !important;
         line-height: 1.3 !important;
-        padding-top: 45px !important;
+        padding-top: 30px !important;
     }}
     
     /* Smartphones button */
