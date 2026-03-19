c02f8fd9fc7bda53c7045495fb6dd6305447d2ca
public-ktolia <k.tolia@pmmretail.com>
2026-03-19 11:15:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.markdown("""
     [data-testid="stSidebar"] {
         border-right: 1px solid #eaeaea !important;
         padding-top: 0 !important;
-        top: 120px !important;
+        top: 20px !important;
     }
     [data-testid="stSidebar"] > div:first-child {
         padding-top: 0 !important;
